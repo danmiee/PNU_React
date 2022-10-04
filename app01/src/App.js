65ecfe8ae7654272020615fc5f6046a70c5ec028
danmiee <dan-mi@kakao.com>
2022-10-04 18:02:29 +0900

    리액트 구조 및 컴포넌트 설명 주석으로 기재

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,6 +1,19 @@
+// 컴포넌트 만들 때 이 형식 참고하기
+/*
+  컴포넌트(사용자정의 태그) : 기존 태그를 내마음대로 조합하는 것
+
+  1. 컴포넌트는 반드시 대문자로 시작
+  2. 컴포넌트 하나는 함수 하나
+    - 함수 여러개 들어가는 경우는 결합임
+  3. 정의된 함수에는 return이 있어야 함
+  4. return에는 기존 html태그가 "하나만" 존재
+    - 자식태그는 넣을 수 있음
+*/
+
 import logo from './logo.svg';
 import './App.css';
 
+// App : 사용자 정의 태그
 function App() {
   return (
     <div className="App">
--- app01/src/index.js
+++ app01/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// 생성한 컴포넌트 import로 등록
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// 기본적인 가상돔 형성
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // 지금부터 리액트 폼에 맞춰서 작성하기
   <React.StrictMode>
+    {/* 컴포넌트(사용자정의 태그) 생성
+      App.js에서 정의
+      내가 만드는 컴포먼트는 반드시 대문자로 시작
+    */}
     <App />
   </React.StrictMode>
 );
